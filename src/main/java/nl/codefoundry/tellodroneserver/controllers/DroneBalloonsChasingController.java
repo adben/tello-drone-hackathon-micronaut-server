341c9ceccbc374eb1bd89b67635610a785386e33
Adolfo Benedetti <adolfo.benedetti-velasquez@ictu.nl>
2023-02-08 12:31:51 +0100

    added video service

--- src/main/java/nl/codefoundry/tellodroneserver/controllers/DroneBalloonsChasingController.java
+++ src/main/java/nl/codefoundry/tellodroneserver/controllers/DroneBalloonsChasingController.java
@@ -1,8 +1,10 @@
 package nl.codefoundry.tellodroneserver.controllers;
 
 import io.micronaut.core.io.ResourceLoader;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
 import jakarta.inject.Inject;
 import me.friwi.tello4j.api.world.MovementDirection;
 import me.friwi.tello4j.api.world.TurnDirection;
@@ -14,12 +16,8 @@ import nl.codefoundry.tellodroneserver.services.DroneVideoService;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.UUID;
-
-import static java.lang.System.out;
+import java.util.stream.IntStream;
 
 @Controller("api/drone/chasing")
 public class DroneBalloonsChasingController {
@@ -37,7 +35,8 @@ public class DroneBalloonsChasingController {
         this.droneFlightService = droneFlightService;
 
         try {
-            defaultVideoFrameOutput = resourceLoader.getResourceAsStream("no-video-output.jpg").get().readAllBytes();
+            defaultVideoFrameOutput = resourceLoader.getResourceAsStream("no-video-output.jpg")
+                    .get().readAllBytes();
         } catch (IOException e) {
             throw new RuntimeException("Failed to load default video frame output", e);
         }
@@ -47,36 +46,35 @@ public class DroneBalloonsChasingController {
     public void balloons() {
         this.droneService.connect();
         this.droneFlightService.takeoff();
-        this.droneFlightService.move(MovementDirection.UP, 160);
-        for (int range = 1; range < 10; range++) {
-            droneFlightService.turn(TurnDirection.LEFT, range * 3);
-            droneVideoService.getLastVideoFrame()
-                    .map(DroneBalloonsChasingController::bufferedImageToJpgByteArray)
-                    .map(byteOutStream -> {
-                        String filename = UUID.randomUUID().toString() + ".jpg";
-                        try (OutputStream outStream = new FileOutputStream(UUID.randomUUID().toString())) {
-                            // writing bytes in to byte output stream
-                            byteOutStream.writeTo(outStream);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        return filename;
-                    })
-                    .ifPresent(out::println);
-
-        }
+        this.droneFlightService.move(MovementDirection.UP, 120);
+        IntStream.range(1, 20)
+                .forEachOrdered(range -> {
+                    droneFlightService.turn(TurnDirection.LEFT, range * 18);
+                    this.getLastVideoFrame();
+                });
         this.droneFlightService.land();
         this.droneService.disconnect();
     }
 
-    private static final ByteArrayOutputStream bufferedImageToJpgByteArray(BufferedImage image) {
+    @Get("foto")
+    @Produces(MediaType.IMAGE_JPEG)
+    public byte[] getLastVideoFrame() {
+        final var lastFrame = droneVideoService.getLastVideoFrame();
+        return lastFrame.map(DroneBalloonsChasingController::bufferedImageToJpgByteArray)
+                .orElse(defaultVideoFrameOutput);
+
+    }
+
+    private static byte[] bufferedImageToJpgByteArray(BufferedImage image) {
         final var out = new ByteArrayOutputStream();
+
         try {
             ImageIO.write(image, "jpg", out);
         } catch (IOException e) {
             throw new DroneException("Failed to convert BufferedImage to JPG byte array", e);
         }
-        return out;
+
+        return out.toByteArray();
     }
 
 
