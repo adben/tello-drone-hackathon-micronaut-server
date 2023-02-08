a5b9fa88f584ef61753a9590cf9faa7727242e9b
Adolfo Benedetti <adolfo.benedetti-velasquez@ictu.nl>
2023-02-08 17:31:37 +0100

    closing it

--- src/main/java/nl/codefoundry/tellodroneserver/controllers/DroneBalloonsChasingController.java
+++ src/main/java/nl/codefoundry/tellodroneserver/controllers/DroneBalloonsChasingController.java
@@ -6,15 +6,20 @@ import io.micronaut.http.annotation.Get;
 import jakarta.inject.Inject;
 import me.friwi.tello4j.api.world.MovementDirection;
 import me.friwi.tello4j.api.world.TurnDirection;
+import nl.codefoundry.tellodroneserver.exceptions.DroneException;
 import nl.codefoundry.tellodroneserver.services.DroneFlightService;
 import nl.codefoundry.tellodroneserver.services.DroneService;
 import nl.codefoundry.tellodroneserver.services.DroneVideoService;
 import nl.codefoundry.tellodroneserver.services.ImageRecognitionUtil;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.stream.IntStream;
 
 @Controller("api/drone/chasing")
 public class DroneBalloonsChasingController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DroneBalloonsChasingController.class);
 
     private final DroneService droneService;
     private final DroneFlightService droneFlightService;
@@ -34,25 +39,25 @@ public class DroneBalloonsChasingController {
         try {
             droneFlightService.move(MovementDirection.UP, 120);
             rotateAndFwd();
-        } catch (Exception e) {
-            System.out.println(e);
-            this.droneFlightService.emergencyStop();
-        } finally {
             this.droneFlightService.land();
+        } catch (Exception e) {
+            throw new DroneException(e.getMessage());
+        } finally {
+            this.droneFlightService.emergencyStop();
         }
     }
 
     private void rotateAndFwd() {
         IntStream.range(1, 20).forEachOrdered(range -> {
             this.droneVideoService.getLastVideoFrame()
-                .ifPresentOrElse(image -> {
-                    if (ImageRecognitionUtil.isBalloonInImage(image)) {
-                        droneFlightService.move(MovementDirection.FORWARD, 50);
-                        rotateAndFwd();
-                    }
-                }, () -> droneFlightService.turn(TurnDirection.LEFT, 18));
+                    .ifPresentOrElse(image -> {
+                        if (ImageRecognitionUtil.isBalloonInImage(image)) {
+                            droneFlightService.move(MovementDirection.FORWARD, 50);
+                            rotateAndFwd();
+                        }
+                    }, () -> droneFlightService.turn(TurnDirection.LEFT, 18));
         });
-        
+
     }
 
 }
